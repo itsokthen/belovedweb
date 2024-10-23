434d4080a3e4cd22c68ebf32684c8cacec9c3b92
itsokthen <cades117@gmail.com>
2024-10-23 18:29:20 -0500

    added Cloudinary to use for Banner images

--- src/app/components/ImageSection.tsx
+++ src/app/components/ImageSection.tsx
@@ -2,36 +2,38 @@
 import React from "react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { CldImage } from "next-cloudinary";
 
 const ImageSection = () => {
   const pathname = usePathname();
 
   let sectionText = "Home";
-  let imageUrl = "/home-banner.png";
+  let imageUrl = "l8pfzobotd38bii0egt5";
 
   if (pathname.startsWith("/our-story")) {
     sectionText = "Our Story";
-    imageUrl = "/story-banner.jpg";
+    imageUrl = "ia71xwr8txc35a5tysnf";
   } else if (pathname.startsWith("/our-pets")) {
     sectionText = "Our Pets";
-    imageUrl = "/pets-banner.png";
+    imageUrl = "z4rprngb0p4y43b1z2lm";
   } else if (pathname.startsWith("/our-vision")) {
     sectionText = "Our Vision";
-    imageUrl = "/vision-banner.png";
+    imageUrl = "lhwe2xbhnvrwdquqsvx2";
   } else if (pathname.startsWith("/Donate")) {
     sectionText = "Contribute";
-    imageUrl = "/donate-banner.png";
+    imageUrl = "va8g1csqh00vi2vk68ap";
   }
 
   return (
     <section>
       <div className="relative w-full h-[200px] lg:h-[250px] xl:h-[300px] 2xl:h-[325px]">
-        <Image
+        <CldImage
           src={imageUrl}
           alt={`${sectionText} Banner Image`}
           fill
           className="object-cover"
           style={{ objectPosition: "center 50%" }}
+          sizes="100vw"
         />
         <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-[40px] lg:text-[60px] font-bold text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           {sectionText}
